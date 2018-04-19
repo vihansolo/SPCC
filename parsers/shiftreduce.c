85219526ad34497af1b52899f48165a3c5dde265
Vihang Garud <garudvihang0@gmail.com>
2018-04-19 06:58:51 +0530

    Debugged shifreduce.c

--- parsers/shiftreduce.c
+++ parsers/shiftreduce.c
@@ -3,7 +3,7 @@
 #include<string.h>
 
 int k=0,z=0,i=0,j=0,c=0;
-char a[16],ac[20],stk[15],act[10];
+char a[50],ac[50],stk[50],act[50];
 
 void check();
 
@@ -73,7 +73,7 @@ void check() {
      {
        stk[z]='E';
        stk[z+1]='\0';
-       stk[z+1]='\0';
+       stk[z+2]='\0';
        printf("\n$%s\t\t%s$\t\t%s",stk,a,ac);
        i=i-2;
      }
