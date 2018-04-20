ef6719113ab56230f06805b024a36302c43ca8c6
Vihang Garud <garudvihang0@gmail.com>
2018-04-20 23:51:48 +0530

    OCD Changes for lex.c

--- Lextool/lex.c
+++ Lextool/lex.c
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 
 // Returns 'true' if the character is a DELIMITER.
-bool isDelimiter(char ch)
-{
+bool isDelimiter(char ch) {
+
     if (ch == ' ' || ch == '+' || ch == '-' || ch == '*' ||
         ch == '/' || ch == ',' || ch == ';' || ch == '>' ||
         ch == '<' || ch == '=' || ch == '(' || ch == ')' ||
@@ -15,8 +15,8 @@ bool isDelimiter(char ch)
 }
 
 // Returns 'true' if the character is an OPERATOR.
-bool isOperator(char ch)
-{
+bool isOperator(char ch) {
+
     if (ch == '+' || ch == '-' || ch == '*' ||
         ch == '/' || ch == '>' || ch == '<' ||
         ch == '=')
@@ -25,19 +25,20 @@ bool isOperator(char ch)
 }
 
 // Returns 'true' if the string is a VALID IDENTIFIER.
-bool validIdentifier(char* str)
-{
+bool validIdentifier(char* str) {
+
     if (str[0] == '0' || str[0] == '1' || str[0] == '2' ||
         str[0] == '3' || str[0] == '4' || str[0] == '5' ||
         str[0] == '6' || str[0] == '7' || str[0] == '8' ||
         str[0] == '9' || isDelimiter(str[0]) == true)
-        return (false);
+          return (false);
+
     return (true);
 }
 
 // Returns 'true' if the string is a KEYWORD.
-bool isKeyword(char* str)
-{
+bool isKeyword(char* str) {
+
     if (!strcmp(str, "if") || !strcmp(str, "else") ||
         !strcmp(str, "while") || !strcmp(str, "do") ||
         !strcmp(str, "break") ||
@@ -50,13 +51,14 @@ bool isKeyword(char* str)
         || !strcmp(str, "switch") || !strcmp(str, "unsigned")
         || !strcmp(str, "void") || !strcmp(str, "static")
         || !strcmp(str, "struct") || !strcmp(str, "goto"))
-        return (true);
+          return (true);
+
     return (false);
 }
 
 // Returns 'true' if the string is an INTEGER.
-bool isInteger(char* str)
-{
+bool isInteger(char* str) {
+
     int i, len = strlen(str);
 
     if (len == 0)
@@ -66,8 +68,9 @@ bool isInteger(char* str)
             && str[i] != '3' && str[i] != '4' && str[i] != '5'
             && str[i] != '6' && str[i] != '7' && str[i] != '8'
             && str[i] != '9' || (str[i] == '-' && i > 0))
-            return (false);
+              return (false);
     }
+
     return (true);
 }
 
@@ -79,50 +82,59 @@ bool isRealNumber(char* str)
 
     if (len == 0)
         return (false);
+
     for (i = 0; i < len; i++) {
+
         if (str[i] != '0' && str[i] != '1' && str[i] != '2'
             && str[i] != '3' && str[i] != '4' && str[i] != '5'
             && str[i] != '6' && str[i] != '7' && str[i] != '8'
             && str[i] != '9' && str[i] != '.' ||
             (str[i] == '-' && i > 0))
-            return (false);
+              return (false);
+
         if (str[i] == '.')
             hasDecimal = true;
     }
+
     return (hasDecimal);
 }
 
 // Extracts the SUBSTRING.
-char* subString(char* str, int left, int right)
-{
+char* subString(char* str, int left, int right) {
+
     int i;
-    char* subStr = (char*)malloc(
-                  sizeof(char) * (right - left + 2));
+    char* subStr = (char*)malloc(sizeof(char) * (right - left + 2));
 
     for (i = left; i <= right; i++)
         subStr[i - left] = str[i];
+
     subStr[right - left + 1] = '\0';
+
     return (subStr);
 }
 
 // Parsing the input STRING.
-void parse(char* str)
-{
+void parse(char* str) {
+
     int left = 0, right = 0;
     int len = strlen(str);
 
     while (right <= len && left <= right) {
+
         if (isDelimiter(str[right]) == false)
             right++;
 
         if (isDelimiter(str[right]) == true && left == right) {
+
             if (isOperator(str[right]) == true)
                 printf("'%c' IS AN OPERATOR\n", str[right]);
 
             right++;
             left = right;
-        } else if (isDelimiter(str[right]) == true && left != right
-                   || (right == len && left != right)) {
+        }
+
+        else if (isDelimiter(str[right]) == true && left != right || (right == len && left != right)) {
+
             char* subStr = subString(str, left, right - 1);
 
             if (isKeyword(subStr) == true)
@@ -144,15 +156,17 @@ void parse(char* str)
             left = right;
         }
     }
+
     return;
 }
 
 // DRIVER FUNCTION
-int main()
-{
+int main() {
+
      // maximum legth of string is 100 here
     char str[100] = "int a = b + 1c; ";
 
+    printf("\n");
     parse(str); // calling the parse function
 
     return (0);
